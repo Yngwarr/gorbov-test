8e9171a86e9c9755e8b6d4bd70d3dd458d6caba3
Yngwarr <k.yngvarr@gmail.com>
2018-09-25 22:04:08 +0700

    Board object

---
+++ js/board.js
@@ -0,0 +1,74 @@
+class Board {
+	constructor() {
+		this.current = 1;
+		this.init();
+	}
+	// creates buttons in the board
+	init() {
+		let brd = document.getElementById('board');
+		// populate the brd with buttons
+		for (let i = 0; i < SIDE; ++i) {
+			let row = document.createElement('div');
+			row.classList.add('row');
+			for (let j = 0; j < SIDE; ++j) {
+				let btn = document.createElement('button');
+				btn.appendChild(document.createTextNode(' '));
+				btn.classList.add(`roll-${j+i+1}`, 'blank');
+				btn.addEventListener('click', (e) => {
+					this.btn_click.call(this, e);
+				});
+				row.appendChild(btn);
+			}
+			brd.appendChild(row);
+		}
+	}
+	btn_click(e) {
+		const num = parseInt(e.target.innerText);
+		if (num !== board.current) return;
+		e.target.disabled = true;
+		++board.current;
+	}
+	// fills buttons with random numbers
+	populate() {
+		let btns = document.querySelectorAll('#board button');
+		let r = _.shuffle(_.range(1,SIDE**2+1));
+		for (let i = 0; i < SIDE**2; ++i) {
+			btns[i].innerText = r.pop();
+		}
+		this.current = 1;
+	}
+	// wipes a board and repopulates it
+	reset() {
+		this.roll((b, fwd) => {
+			if (fwd) b.classList.add('blank');
+			else b.classList.remove('blank');
+		}, () => { this.populate.call(this) });
+		this.current = 1;
+	}
+	wipe_roll() {
+		this.roll((b, fwd) => { b.classList.add('blank'); });
+	}
+	unwipe_roll() {
+		this.roll((b, fwd) => { if (!fwd) b.classList.remove('blank'); })
+	}
+	roll(switch_cb, wave_cb) {
+		let f = (n, fwd, switch_cb, wave_cb) => {
+			let line = document.querySelectorAll(`#board button.roll-${n}`);
+			for (let i = 0; i < line.length; ++i) {
+				line[i].disabled = !!fwd;
+				if (switch_cb) switch_cb(line[i], fwd);
+			}
+			// set the second wave up
+			if (n === SIDE*2 && fwd) {
+				if(wave_cb) wave_cb();
+				setTimeout(f, 50, 1, false, switch_cb, wave_cb);
+				return;
+			}
+			// roll the next layer
+			if (n < SIDE*2) {
+				setTimeout(f, 50, n+1, fwd, switch_cb, wave_cb);
+			}
+		}
+		f(1, true, switch_cb, wave_cb);
+	}
+}
--- js/main.js
+++ js/main.js
@@ -1,68 +1,7 @@
 const SIDE = 5
-let current = 1;
+
+let board;
 
 function init() {
-	fill_board();
-	fill_btns()
-}
-
-// creates buttons in the board
-function fill_board() {
-	let board = document.getElementById('board');
-	// populate the board with buttons
-	for (let i = 0; i < SIDE; ++i) {
-		let row = document.createElement('div');
-		row.classList.add('row');
-		for (let j = 0; j < SIDE; ++j) {
-			let btn = document.createElement('button');
-			btn.appendChild(document.createTextNode(' '));
-			btn.classList.add(`roll-${j+i+1}`);
-			btn.addEventListener('click', btn_click);
-			row.appendChild(btn);
-		}
-		board.appendChild(row);
-	}
-}
-
-// fills buttons with random numbers
-function fill_btns() {
-	let btns = document.querySelectorAll('#board button');
-	let r = _.shuffle(_.range(1,SIDE**2+1));
-	for (let i = 0; i < SIDE**2; ++i) {
-		btns[i].innerText = r.pop();
-	}
-	current = 1;
-}
-
-function btn_click(e) {
-	const num = parseInt(e.target.innerText);
-	if (num !== current) return;
-	e.target.disabled = true;
-	++current;
-}
-
-function roll(wipe) {
-	let f = (n, fwd, switch_cb, wave_cb) => {
-		let line = document.querySelectorAll(`#board button.roll-${n}`);
-		for (let i = 0; i < line.length; ++i) {
-			line[i].disabled = !!fwd;
-			if (switch_cb) switch_cb(line[i], fwd);
-		}
-		// set the second wave up
-		if (n === SIDE*2 && fwd) {
-			if(wave_cb) wave_cb();
-			setTimeout(f, 50, 1, false, switch_cb, wave_cb);
-			return;
-		}
-		// roll the next layer
-		if (n < SIDE*2) {
-			setTimeout(f, 50, n+1, fwd, switch_cb, wave_cb);
-		}
-	}
-	//if (wipe) f(1, true, (b, fwd) => { b.classList.add('blank'); });
-	//else f(1, true, (b, fwd) => { if (!fwd) b.classList.remove('blank'); });
-	f(1, true, (b, fwd) => {
-		if (fwd) b.classList.add('blank');
-		else b.classList.remove('blank');
-	}, fill_btns);
+	board = new Board();
 }
