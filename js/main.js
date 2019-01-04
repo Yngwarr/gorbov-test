3a07cce7a638e6342b588f745d9256c6cd485c01
Yngwarr <k.yngvarr@gmail.com>
2019-01-04 15:45:54 +0700

    session control

--- js/main.js
+++ js/main.js
@@ -12,12 +12,19 @@ function init() {
 	document.getElementById('btn-login').addEventListener('click', (e) => {
 		let username = document.getElementById('in-username').value;
 		let password = document.getElementById('in-password').value;
-		if (session.login(username, password)) {
+		let res = session.login(username, password);
+		if (res[0]) {
 			// TODO alter page layout for logged in user
+			alter_page();
 			hide_modal('win-login');
 		} else {
 			// TODO add an error notification, ideally with a message
 		}
+	});
+
+	document.getElementById('sign-out').addEventListener('click', (e) => {
+		session.logout();
+		alter_page();
 	});
 
 	//start();
@@ -40,9 +47,26 @@ function fail(btn) {
 }
 
 function show_modal(win) {
+	if (win === 'win-login') {
+		let ins = document.querySelectorAll(`#${win} input`);
+		ins.forEach(e => e.value = "");
+		setTimeout(() => { ins[0].focus(); }, 0);
+	}
 	document.getElementById(win).classList.remove('hidden');
 }
 
 function hide_modal(win) {
 	document.getElementById(win).classList.add('hidden');
+}
+
+function alter_page() {
+	if (session.user !== null) {
+		document.getElementById('sign-out').classList.remove('hidden');
+		document.getElementById('sign-in').classList.add('hidden');
+		document.getElementById('status').textContent = `Привет, ${session.user}!`;
+	} else {
+		document.getElementById('sign-out').classList.add('hidden');
+		document.getElementById('sign-in').classList.remove('hidden');
+		document.getElementById('status').textContent = '';
+	}
 }
--- js/session.js
+++ js/session.js
@@ -3,35 +3,38 @@ class Session {
 		this.user = null;
 	}
 	login(name, passwd) {
+		if (this.user !== null) return [false, "logged in"];
 		if (!localStorage.users) {
 			localStorage.users = "[]";
 			/* can't find the user with no users at all */
-			return false;
+			return [false, "wrong credentials"];
 		}
 		let users = JSON.parse(localStorage.users);
 		let usr = users.filter((u) => {
 			return u.n === name && u.p === sha256_digest(passwd);
 		});
 		/* user not found */
-		if (usr.length === 0) return false;
+		if (usr.length === 0) return [false, "wrong credentials"];
 		this.user = usr[0].n;
-		return true;
+		return [true];
 	}
 	logout() {
 		this.user = null;
+		return [true];
 	}
 	sign_up(name, passwd) {
 		/* sanity check */
-		if (name.length === 0 || passwd.length === 0) return false;
+		if (name.length === 0) return [false, "no name"];
+		if (passwd.length === 0) return [false, "no password"];
 		if (!localStorage.users) { localStorage.users = "[]"; }
 
 		let users = JSON.parse(localStorage.users);
 		let usr = users.filter((u) => { return u.n === name; });
 		/* such user exists */
-		if (usr.length > 0) return false;
+		if (usr.length > 0) return [false, "name exists"];
 		users.push({ n: name, p: sha256_digest(passwd) });
 		localStorage.users = JSON.stringify(users);
 		this.user = name;
-		return true;
+		return [true];
 	}
 }
